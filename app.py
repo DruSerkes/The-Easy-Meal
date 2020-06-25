9d0d8af6d49735e7eb4da26cdb0b7b7ba6bad406
Dru Serkes <DruSerkes@gmail.com>
2020-06-25 02:27:17 -0400

    Add user to g before request. Add helpers do_login. do_logout.

--- app.py
+++ app.py
@@ -19,6 +19,34 @@ db.create_all()
 toolbar = DebugToolbarExtension(app)
 
 
+#####################################
+#     User Signup/Login/Logout      #
+#####################################
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
 ########################
 #       Routes         #
 ########################
