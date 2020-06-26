1621ebfbdf06f9fae1de1f63ba4c3ad2f70b637f
Dru Serkes <DruSerkes@gmail.com>
2020-06-26 14:32:54 -0400

    seed.py imports Step

--- seed.py
+++ seed.py
@@ -1,6 +1,6 @@
 """ Seed file to make sample data for pets db """
 
-from models import User, Recipe, Measurement, Ingredient, db
+from models import User, Recipe, Measurement, Ingredient, Step, db
 from app import app
 
 # Create all tables
