a3f34f29b294f92888733dd90bf8d6a4633e0b02
Dru Serkes <DruSerkes@gmail.com>
2020-06-27 15:27:03 -0400

    generateModalHTML: added helper function

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -19,6 +19,27 @@ async function addIngredientsToGroceryList(evt) {
 	response = await axios.post(`/groceries`, (data = { id }));
 	// Display response on DOM
 	console.log(response.data.message);
+	const modalHTML = generateModalHTML(response.data);
 }
 
-
+function generateModalHTML(data) {
+	return `<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">${data.message}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <a class="btn btn-primary text-white" href="{{ url_for('view_grocery_list') }}">Go to Shopping List</a>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>`;
+}
