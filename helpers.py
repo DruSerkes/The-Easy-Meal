354977405126e6a1ac04668c6b35154e47836be6
Dru Serkes <DruSerkes@gmail.com>
2020-07-05 03:27:15 -0400

    try os.environ.get for secret key

--- helpers.py
+++ helpers.py
@@ -3,11 +3,12 @@ from secrets import student_key
 from models import User, db, Recipe, Ingredient, Measurement, Step
 from flask import request, session
 import requests
+import os
 
 
 CURR_USER_KEY = "user_id"
 API_BASE_URL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-API_KEY = student_key
+API_KEY = os.environ.get('student_key', student_key)
 
 
 valid_cuisines = ['african', 'chinese', 'japanese', 'korean', 'vietnamese', 'thai', 'indian', 'british', 'irish', 'french', 'italian', 'mexican',
